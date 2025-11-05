7b0228e20cdafc494b5ff41ced94c4335e2d9108
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-11-05 12:23:50 +0545

    task4 completed

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Console.WriteLine("Hello, from task 4");
+class Program
+{
+    static void Main()
+    {
+        int[] myFavNumbers = { 9,8,7,6,5 };
+        Array.Sort(myFavNumbers);
+        Array.Reverse(myFavNumbers);
+
+        Console.WriteLine("Desending order:");
+        for (int i = 0; i < myFavNumbers.Length; i++)
+        {
+            Console.WriteLine(myFavNumbers[i]);
+        }
+        int findNumber = 5;
+        int index = Array.IndexOf(myFavNumbers, findNumber);
+
+        Console.WriteLine($"\nPosition of {findNumber} in array: {index}");
+    }
+}
