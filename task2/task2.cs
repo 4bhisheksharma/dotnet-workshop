cea1f9b4f679e225798f786bbadce4f7c83166b3
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-11-05 12:05:03 +0545

    task2 completed

---
+++ task2/task2.cs
@@ -0,0 +1,31 @@
+﻿
+class Circle
+{
+    // constant variable
+    public const double PI = 3.14;
+
+    // method to calculate area
+    public double CalculateArea(double radius)
+    {
+        return PI * radius * radius;
+    }
+
+    // method to calculate perimeter
+    public double CalculatePerimeter(double radius)
+    {
+        return 2 * PI * radius;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //Circle.PI = 3.15; // This line will cause a compile-time error cause pi is constant and we can change the constant value
+        Circle c = new Circle();
+        double radius = 5;
+
+        Console.WriteLine("Area of Circle: " + c.CalculateArea(radius));
+        Console.WriteLine("Perimeter of Circle: " + c.CalculatePerimeter(radius));
+    }
+}
